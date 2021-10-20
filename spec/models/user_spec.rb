b9052d93535bc720e095f456484c6f181ba7aa9e
Nicholas Timbo <nicholasnogueira12@gmail.com>
2021-10-20 22:40:24 +0000

    Ajustes da tela home, criando testes com a gem FactoryBot, adicionando o campo nome para o usuario e validando.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it "is valid" do
-    user = User.new(email: "user@user.com", password: "123456")
-    expect(user.email).to eql "user@user.com"
+    user = User.new(email: "user@user.com", password: "123456", name: "User")
+    # expect(user.email).to eql "user@user.com"
     expect(user).to be_valid
   end
 
@@ -11,4 +11,10 @@ RSpec.describe User, type: :model do
     user = User.new(email: "user@user.com", password: "")
     expect(user).to_not be_valid
   end
+
+  it "should have a name" do
+    user = User.new(email: "user@user.com", password: "123456")
+    expect(user).to_not be_valid
+  end
+
 end
--- vendor/assets/javascripts/scripts.js
+++ vendor/assets/javascripts/scripts.js
@@ -4,7 +4,6 @@
 // Collapse the navbar by adding the top-nav-collapse class
 window.onscroll = function () {
 	scrollFunction();
-	scrollFunctionBTT(); // back to top button
 };
 
 window.onload = function () {
@@ -18,19 +17,6 @@ function scrollFunction() {
 		document.getElementById("navbar").classList.remove("top-nav-collapse");
 	}
 }
-
-// Navbar on mobile
-let elements = document.querySelectorAll(".nav-link:not(.dropdown-toggle)");
-
-for (let i = 0; i < elements.length; i++) {
-	elements[i].addEventListener("click", () => {
-		document.querySelector(".offcanvas-collapse").classList.toggle("open");
-	});
-}
-
-document.querySelector(".navbar-toggler").addEventListener("click", () => {
-  	document.querySelector(".offcanvas-collapse").classList.toggle("open");
-});
 
 // Hover on desktop
 function toggleDropdown(e) {
@@ -49,28 +35,6 @@ function toggleDropdown(e) {
 	);
 }
 
-// On hover
-const dropdownCheck = document.querySelector('.dropdown');
-
-if (dropdownCheck !== null) { 
-	document.querySelector(".dropdown").addEventListener("mouseleave", toggleDropdown);
-	document.querySelector(".dropdown").addEventListener("mouseover", toggleDropdown);
-
-	// On click
-	document.querySelector(".dropdown").addEventListener("click", (e) => {
-		const _d = e.target.closest(".dropdown");
-		let _m = document.querySelector(".dropdown-menu", _d);
-		if (_d.classList.contains("show")) {
-			_m.classList.remove("show");
-			_d.classList.remove("show");
-		} else {
-			_m.classList.add("show");
-			_d.classList.add("show");
-		}
-	});
-}
-  
-
 /* Card Slider - Swiper */
 var cardSlider = new Swiper('.card-slider', {
 	autoplay: {
@@ -84,16 +48,7 @@ var cardSlider = new Swiper('.card-slider', {
 	}
 });
 
-
-/* Filter - Isotope */
-const gridCheck = document.querySelector('.grid');
-
-if (gridCheck !== null) { 
-	// init Isotope
-	var iso = new Isotope( '.grid', {
-		itemSelector: '.element-item',
-		layoutMode: 'fitRows'
-	});
+if (gridCheck !== null) {
 
 	// bind filter button click
 	var filtersElem = document.querySelector('.filters-button-group');
@@ -106,14 +61,14 @@ if (gridCheck !== null) {
 		// use matching filter function
 		iso.arrange({ filter: filterValue });
 	});
-	
+
 	// change is-checked class on buttons
 	var buttonGroups = document.querySelectorAll('.button-group');
 	for ( var i=0, len = buttonGroups.length; i < len; i++ ) {
 		var buttonGroup = buttonGroups[i];
 		radioButtonGroup( buttonGroup );
 	}
-	
+
 	function radioButtonGroup( buttonGroup ) {
 		buttonGroup.addEventListener( 'click', function( event ) {
 			// only work with buttons
@@ -124,24 +79,4 @@ if (gridCheck !== null) {
 			event.target.classList.add('is-checked');
 		});
 	}
-}
-
-
-/* Back To Top Button */
-// Get the button
-myButton = document.getElementById("myBtn");
-
-// When the user scrolls down 20px from the top of the document, show the button
-function scrollFunctionBTT() {
-	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-		myButton.style.display = "block";
-	} else {
-		myButton.style.display = "none";
-	}
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-	document.body.scrollTop = 0; // for Safari
-	document.documentElement.scrollTop = 0; // for Chrome, Firefox, IE and Opera
 }
